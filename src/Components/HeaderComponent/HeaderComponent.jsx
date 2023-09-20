de78ad950f712f3c5196f476e997222f73f5b88d
Đặng Nguyễn Minh An <nnbngan7501@gmail.com>
2023-09-20 15:00:25 +0700

    add login register UI

--- src/Components/HeaderComponent/HeaderComponent.jsx
+++ src/Components/HeaderComponent/HeaderComponent.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const HeaderComponent = () => {
-  return (
-    <div>HeaderComponent</div>
-  )
+    const navLinks = [
+        {
+            path: "/",
+            display: "Home",
+        },
+        {
+            path: "/service",
+            display: "Service",
+        },
+        {
+            path: "/login",
+            display: "Log in",
+        },
+    ];
+
+    return (
+        <div className="menu">
+            {navLinks.map((item, index) => (
+                <NavLink
+                    to={item.path}
+                    key={index}
+                >
+                    {item.display}
+                </NavLink>
+            ))}
+        </div>
+    )
 }
 
 export default HeaderComponent
--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import AuthenticationComponent from '../../Components/AuthenticationComponent'
+import { BiAt } from "react-icons/bi"
+import { RiLockPasswordFill } from "react-icons/ri"
+import { IoPersonCircleSharp } from "react-icons/io5"
+import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthenticationComponent>
+      <div className='content'>
+        <h3>Create account</h3>
+        <form className="login__form-content">
+          <div className="input-box">
+            <IoPersonCircleSharp />
+            <input type="password" className='input'
+              placeholder=" "
+            />
+            <label htmlFor="">Fullname</label>
+          </div>
+
+          <div className="input-box">
+            <BiAt />
+            <input type="text" className='input'
+              placeholder=" "
+            />
+            <label htmlFor="">Email address</label>
+          </div>
+
+          <div className="input-box">
+            <RiLockPasswordFill />
+            <input type="password" className='input'
+              placeholder=" "
+            />
+
+            <label htmlFor="">Password</label>
+          </div>
+
+          <div className="input-box">
+            <RiLockPasswordFill />
+            <input type="password" className='input'
+              placeholder=" "
+            />
+
+            <label htmlFor="">Confirm Password</label>
+          </div>
+
+          <div className='password-feature'>
+            <div className='remember'>
+              <input type='checkbox'></input>
+              <p>I agree with <span>Terms</span> and <span>Privacy</span></p>
+            </div>
+
+          </div>
+
+          <div className='btn-login'>Sign up</div>
+
+          <p className='text'>Already have an account?
+            <Link to='/login' className="text-swap" > Log in</Link>
+          </p>
+        </form>
+      </div>
+    </AuthenticationComponent>
   )
 }
 
