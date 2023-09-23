58953acecf16443081a3068282fd7f9c8efb53d7
Doan Hoang Huy <92454872+HwiHwy@users.noreply.github.com>
2023-09-23 07:35:48 +0700

    service

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -44,7 +44,7 @@ const LoginPage = () => {
             
           </div>
 
-          <div className='btn-login'>Sign in</div>
+          <div className='btn-login'>  Sign in</div>
           <div className='btn-login btn-login-google'>
             <FcGoogle />
             <p>Sign in with Google</p>
---
+++ src/Pages/Service/Service.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroComponent from '../../Components/HeroComponent'
+
+const ServicePage = () => {
+  return (
+    <div>
+              <HeroComponent />
+              <div className='t-10'>
+                adsadaw
+              </div>
+       </div>
+  )
+}
+
+export default ServicePage
