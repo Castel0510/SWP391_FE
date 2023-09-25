8c5163a31e9815064debddee0cf8f048f482a111
NganNguyen752001 <nnbngan7501@gmail.com>
2023-09-25 13:20:05 +0700

    add login

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,62 +1,131 @@
-import React, { useState } from 'react'
-import AuthenticationComponent from '../../Components/AuthenticationComponent'
-import ForgotPasswordComponent from '../../Components/ForgotPasswordComponent'
-import { BiAt } from "react-icons/bi"
-import { RiLockPasswordFill } from "react-icons/ri"
-import { FcGoogle } from "react-icons/fc"
-import './style.scss'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import AuthenticationComponent from '../../Components/AuthenticationComponent';
+import ForgotPasswordComponent from '../../Components/ForgotPasswordComponent';
+import InputField from '../../Components/Shared/InputField';
+import { BiAt } from 'react-icons/bi';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { FcGoogle } from 'react-icons/fc';
+import './style.scss';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../../Store/userSlice';
+import { validateEmail } from '../../Utils';
+
 
 const LoginPage = () => {
-
   const [showForgotPasswordModal, setShowForgotPasswordModal] = useState(false);
+
+  // State
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({ email: '', password: '' });
+
+  // Redux state
+  const { loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Reset previous errors
+    setErrors({ email: '', password: '' });
+
+    // Validation
+    let hasErrors = false;
+    if (!email) {
+      setErrors((prevErrors) => ({ ...prevErrors, email: 'Please enter your email!' }));
+      hasErrors = true;
+    } else if (!validateEmail(email)) {
+      setErrors((prevErrors) => ({ ...prevErrors, email: 'Invalid email!' }));
+      hasErrors = true;
+    }
+
+    if (!password) {
+      setErrors((prevErrors) => ({ ...prevErrors, password: 'Please enter your password!' }));
+      hasErrors = true;
+    }
+
+    if (hasErrors) {
+      return;
+    }
+
+    const userCredentials = { email, password };
+
+    dispatch(loginUser(userCredentials)).then((result) => {
+      if (result.payload) {
+        setEmail('');
+        setPassword('');
+        navigate('/');
+      }
+    });
+  };
 
   return (
     <AuthenticationComponent>
-      <div className='content'>
+      <div className="content">
         <h3>Welcome Back!</h3>
-        <form className="login__form-content">
-          <div className="input-box">
-            <BiAt />
-            <input type="text" className='input'
-              placeholder=" "
-            />
-            <label htmlFor="">Email address</label>
-          </div>
+        <form className="login__form-content" onSubmit={handleLogin}>
 
-          <div className="input-box">
-            <RiLockPasswordFill />
-            <input type="password" className='input'
-              placeholder=" "
-            />
+          <InputField
+            icon={BiAt}
+            type="text"
+            placeholder=" "
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email address"
+            error={errors.email}
+          />
 
-            <label htmlFor="">Password</label>
-          </div>
+          <InputField
+            icon={RiLockPasswordFill}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            error={errors.password}
+          />
 
-          <div className='password-feature'>
-            <div className='remember'>
-              <input type='checkbox' id="remember"></input>
+          <div className="password-feature">
+            <div className="remember">
+              <input type="checkbox" id="remember"></input>
               <label htmlFor="remember">Remember me</label>
             </div>
-            <div className='forgot' onClick={() => setShowForgotPasswordModal(true)}>Forgot password?</div>
+            <div className="forgot" onClick={() => setShowForgotPasswordModal(true)}>
+              Forgot password?
+            </div>
 
-            {showForgotPasswordModal ? <ForgotPasswordComponent onClose={() => setShowForgotPasswordModal(false)}/> : <></>}
-            
+            {showForgotPasswordModal ? (
+              <ForgotPasswordComponent onClose={() => setShowForgotPasswordModal(false)} />
+            ) : (
+              <></>
+            )}
           </div>
 
-          <div className='btn-login'>Sign in</div>
-          <div className='btn-login btn-login-google'>
+          {error && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 w-full mt-1 rounded relative" role="alert">
+              <span className="block sm:inline">{error}</span>
+            </div>
+          )}
+
+          <button className="btn-login">{loading ? 'Loading ...' : 'Sign in'}</button>
+
+          <div className="btn-login btn-login-google">
             <FcGoogle />
             <p>Sign in with Google</p>
           </div>
 
-          <p className='text'>Don't have an account? 
-            <Link to='/register' className="text-swap"> Register</Link>
+          <p className="text">
+            Don't have an account?
+            <Link to="/register" className="text-swap">
+              {' '}
+              Register
+            </Link>
           </p>
         </form>
       </div>
     </AuthenticationComponent>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -1,57 +1,167 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthenticationComponent from '../../Components/AuthenticationComponent'
+import InputField from '../../Components/Shared/InputField'
+
 import { BiAt } from "react-icons/bi"
 import { RiLockPasswordFill } from "react-icons/ri"
 import { IoPersonCircleSharp } from "react-icons/io5"
-import { Link } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser, loginUser } from '../../Store/userSlice';
+
 
 const RegisterPage = () => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPwd, setConfirmPwd] = useState('');
+  const [role, setRole] = useState('');
+
+  const [errors, setErrors] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPwd: '',
+    role: ''
+  });
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { loading, error } = useSelector((state) => state.user);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    setErrors({
+      fullName: '',
+      email: '',
+      password: '',
+      confirmPwd: '',
+      role: ''
+    });
+
+    let hasErrors = false;
+
+    if (!fullName) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        fullName: 'Please enter your full name!'
+      }));
+      hasErrors = true;
+    }
+
+    if (!email) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: 'Please enter your email!'
+      }));
+      hasErrors = true;
+    } else if (!validateEmail(email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: 'Invalid email!'
+      }));
+      hasErrors = true;
+    }
+
+    if (!password) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: 'Please enter your password!'
+      }));
+      hasErrors = true;
+    }
+
+    if (password !== confirmPwd) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        confirmPwd: 'Passwords do not match!'
+      }));
+      hasErrors = true;
+    }
+
+    if (!role) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        role: 'Please choose a role!'
+      }));
+      hasErrors = true;
+    }
+
+    if (hasErrors) {
+      return;
+    }
+
+    const userCredentials = {
+      fullName,
+      email,
+      password,
+      role
+    };
+
+    dispatch(createUser(userCredentials));
+
+    dispatch(loginUser(userCredentials)).then((result) => {
+      if (result.payload) {
+        navigate('/')
+      }
+    })
+
+  };
+
   return (
     <AuthenticationComponent>
       <div className='content'>
         <h3>Create account</h3>
-        <form className="login__form-content">
-          <div className="input-box">
-            <IoPersonCircleSharp />
-            <input type="password" className='input'
-              placeholder=" "
-            />
-            <label htmlFor="">Fullname</label>
-          </div>
+        <form className="login__form-content" onSubmit={handleSignup}>
 
-          <div className="input-box">
-            <BiAt />
-            <input type="text" className='input'
-              placeholder=" "
-            />
-            <label htmlFor="">Email address</label>
-          </div>
+          <InputField
+            icon={IoPersonCircleSharp}
+            type="text"
+            value={fullName}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Fullname"
+            error={errors.fullName}
+          />
 
-          <div className="input-box">
-            <RiLockPasswordFill />
-            <input type="password" className='input'
-              placeholder=" "
-            />
+          <InputField
+            icon={BiAt}
+            type="text"
+            placeholder=" "
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email address"
+            error={errors.email}
+          />
 
-            <label htmlFor="">Password</label>
-          </div>
+          <InputField
+            icon={RiLockPasswordFill}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password"
+            error={errors.password}
+          />
 
-          <div className="input-box">
-            <RiLockPasswordFill />
-            <input type="password" className='input'
-              placeholder=" "
-            />
-
-            <label htmlFor="">Confirm Password</label>
-          </div>
+          <InputField
+            icon={RiLockPasswordFill}
+            type="password"
+            value={confirmPwd}
+            onChange={(e) => setConfirmPwd(e.target.value)}
+            label="Confirm Password"
+            error={errors.confirmPwd}
+          />
 
           <div className='dropdown'>
-            <select>
+            <select value={role} onChange={(e) => setRole(e.target.value)}>
               <option value="">Choose option: Customer or Provider</option>
               <option value="customer">Customer</option>
               <option value="provider">Provider</option>
             </select>
           </div>
+
+          {errors.role && <span className="text-red-600">{errors.role}</span>}
 
           <div className='password-feature'>
             <div className='remember'>
@@ -61,7 +171,13 @@ const RegisterPage = () => {
 
           </div>
 
-          <div className='btn-login'>Sign up</div>
+          {error && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 w-full mt-1 rounded relative" role="alert">
+              <span className="block sm:inline">{error}</span>
+            </div>
+          )}
+
+          <button type="submit" className='btn-login'>Sign up</button>
 
           <p className='text'>Already have an account?
             <Link to='/login' className="text-swap" > Log in</Link>
