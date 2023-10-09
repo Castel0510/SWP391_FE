042097551a9b175e10f16ac84ab929f14a72d7bd
ldduc123 <145273434+Castel0510@users.noreply.github.com>
2023-10-09 16:01:45 +0700

    update order history

--- src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/Pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,70 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const OrderHistoryPage = () => {
+  const [selectedStatus, setSelectedStatus] = useState('ALL');
 
   const data = [
     {
-      name: "BIRD HOTEL - ABC HOTEL",
-      dateOrder: " 01/01/2023",
-      dateComplete: "03/01/2023",
-      detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took ",
-      status: "DONE"
+      id: '1',
+      name: 'Make a birdhouse',
+      dateOrder: '01/01/2023',
+      dateComplete: '03/01/2023',
+      detail: 'Craft your custom birdhouse: Build, paint to your garden sanctuary.',
+      status: 'DONE',
     },
     {
-      name: "BIRD HOTEL - ABC HOTEL",
-      dateOrder: " 01/01/2023",
+      id: '2',
+      name: 'Haven Retreat',
+      dateOrder: '01/01/2023',
       dateComplete: null,
-      detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took ",
-      status: "ONGOING"
+      detail: 'Explore birdwatching, nesting, and conservation in our tranquil avian sanctuary.',
+      status: 'ONGOING',
     },
     {
-      name: "BIRD HOTEL - ABC HOTEL",
-      dateOrder: " 01/01/2023",
-      dateComplete: null,
-      detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took ",
-      status: "CANCEL"
+      id: '3',
+      name: 'Feathered Training',
+      dateOrder: '01/01/2023',
+      dateComplete: '03/01/2023',
+      detail: 'Teach and bond with pet birds through professional training and enrichment.',
+      status: 'DONE',
     },
-  ]
+    {
+      id: '4',
+      name: 'Feathered Picture',
+      dateOrder: '01/01/2023',
+      dateComplete: null,
+      detail: 'Capture the beauty of birds in their natural habitat with avian photography services.',
+      status: 'CANCEL',
+    },
+  ];
 
+  const filteredData = selectedStatus === 'ALL' ? data : data.filter((item) => item.status === selectedStatus);
 
+  const getStatusBackgroundColor = (status) => {
+    switch (status) {
+      case 'ONGOING':
+        return 'bg-yellow-300';
+      case 'CANCEL':
+        return 'bg-red-400';
+      case 'DONE':
+        return 'bg-green-400';
+      default:
+        return 'bg-gray-400';
+    }
+  };
 
   return (
     <>
-      <div className="w-fit min-h-[320px] p-8 bg-white shadow-md rounded my-10 mx-auto">
-        <div className='text-center font-bold'>ORDER HISTORY</div>
+      <div className="min-h-[320px] p-8 bg-white shadow-md rounded my-10 mx-auto">
+        <div className="text-center font-bold">ORDER HISTORY</div>
 
-        <div className='mt-5 mb-10'>
-          <button className="bg-gray-400 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded ml-5">
+        <div className="mt-5 mb-10">
+          <button
+            className={`bg-gray-400 hover:bg-gray-500 text-black font-bold py-2 px-4 rounded ml-5 ${
+              selectedStatus === 'ALL' ? 'bg-gray-500' : ''
+            }`}
+            onClick={() => setSelectedStatus('ALL')}
+          >
             All
           </button>
-          <button className="bg-yellow-300 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded ml-5">
+          <button
+            className={`bg-yellow-300 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded ml-5 ${
+              selectedStatus === 'ONGOING' ? 'bg-yellow-500' : ''
+            }`}
+            onClick={() => setSelectedStatus('ONGOING')}
+          >
             ON GOING
           </button>
-          <button className="bg-red-400 hover:bg-red-500 text-black font-bold py-2 px-4 rounded ml-5">
+          <button
+            className={`bg-red-400 hover:bg-red-500 text-black font-bold py-2 px-4 rounded ml-5 ${
+              selectedStatus === 'CANCEL' ? 'bg-red-500' : ''
+            }`}
+            onClick={() => setSelectedStatus('CANCEL')}
+          >
             CANCEL
           </button>
-          <button className="bg-green-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded ml-5">
+          <button
+            className={`bg-green-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded ml-5 ${
+              selectedStatus === 'DONE' ? 'bg-green-500' : ''
+            }`}
+            onClick={() => setSelectedStatus('DONE')}
+          >
             DONE
           </button>
         </div>
 
-        {data && data?.map((item, index) => (
-          <div className='shadow-md rounded-md mt-5' key={index}>
-            <div className='p-4 bg-green-300 rounded-md'>{item.name}</div>
-            <div className='p-3 flex'><p className='font-bold pr-1'>Date order: </p> {item.dateOrder}</div>
-            <div className='p-3 flex'><p className='font-bold pr-1'>Date completed: </p> {item.dateComplete}</div>
-            <div className='p-3 flex leading-normal'><p className='font-bold pr-1'>Detail: </p> {item.detail}</div>
-            <div className='p-3 pb-6 flex'><p className='font-bold pr-1'>Status: </p> {item.status}</div>
+        {filteredData.map((item, index) => (
+          <div className="shadow-md rounded-md mt-5" key={index}>
+            <div className={`p-4 rounded-md ${getStatusBackgroundColor(item.status)}`}>{item.name}</div>
+            <div className="p-3 flex">
+              <p className="font-bold pr-1">Date order: </p> {item.dateOrder}
+            </div>
+            <div className="p-3 flex">
+              <p className="font-bold pr-1">Date completed: </p> {item.dateComplete}
+            </div>
+            <div className="p-3 flex leading-normal">
+              <p className="font-bold pr-1">Detail: </p> {item.detail}
+            </div>
+            <div className="p-3 pb-6 flex">
+              <p className="font-bold pr-1">Status: </p> {item.status}
+            </div>
           </div>
         ))}
-
-
-
       </div>
-
-
     </>
-  )
-}
+  );
+};
 
-export default OrderHistoryPage
+export default OrderHistoryPage;
