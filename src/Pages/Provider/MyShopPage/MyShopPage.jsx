d731902a618d03ed84703ce69506140011dbfff8
Vinh Nhan <vinhnhan1002@gmail.com>
2023-11-02 17:15:05 +0700

    feat my shop page

--- src/Pages/Provider/MyShopPage/MyShopPage.jsx
+++ src/Pages/Provider/MyShopPage/MyShopPage.jsx
@@ -2,103 +2,43 @@ import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { renderRatingStars } from '../../../Utils';
 import axios from 'axios';
+import { useQuery } from 'react-query';
+import Pagination from '@mui/material/Pagination';
 
 const MyShopPage = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [data, setData] = useState([]);
-
-    const [currentPage, setCurrentPage] = useState(1); // Current page number
-
     const items = JSON.parse(localStorage.getItem('userInfo'));
     // console.log("check id: ", items?.id);
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(9);
+    const [totalPage, setTotalPage] = useState(0);
 
-    const GetByProviderId = `https://apis20231023230305.azurewebsites.net/api/BirdService/GetByProviderId?id=${items?.id}`;
+    const serviceQuery = useQuery(
+        ['service', items?.id, page, pageSize],
+        async () => {
+            const res = await axios.get(
+                `https://apis20231023230305.azurewebsites.net/api/BirdService/GetByProviderId?id=${items?.id}`
+            );
 
-    const fetchData = async () => {
-        try {
-            setIsLoading(true);
+            setTotalPage(Math.ceil(res.data.result.length / pageSize));
 
-            const response = await axios.get(GetByProviderId);
-            setData(response.data.result);
-
-            setIsLoading(false);
-        } catch (error) {
-            setError(error.message);
-            setIsLoading(false);
+            return res.data.result.slice((page - 1) * pageSize, page * pageSize);
+        },
+        {
+            initialData: [],
         }
-    };
-
-    // console.log(data);
+    );
 
     const handleDelete = async (id) => {
         try {
-            setIsLoading(true);
-
             const response = await axios.delete(
                 `https://apis20231023230305.azurewebsites.net/api/BirdService/Delete?id=${id}`
             );
 
-            console.log('Delete response:', response.data);
-
-            fetchData();
-
-            setIsLoading(false);
+            serviceQuery.refetch();
         } catch (error) {
             console.error('Delete error:', error);
-            setIsLoading(false);
         }
     };
-
-    useEffect(() => {
-        fetchData();
-        handleDelete();
-    }, []);
-
-    // Number of items to display per page
-    const itemsPerPage = 9;
-    // Calculate the indexes of the items to display on the current page
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-
-    // Get the items for the current page
-    const currentItems = data.slice(startIndex, endIndex);
-
-    // Calculate the total number of pages
-    const totalPages = Math.ceil(data.length / itemsPerPage);
-
-    // Function to handle page navigation
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
-
-    if (isLoading) {
-        return (
-            <div role="status" className="flex justify-center items-center min-h-[600px]">
-                <svg
-                    aria-hidden="true"
-                    className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                    />
-                    <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                    />
-                </svg>
-                <span className="sr-only">Loading...</span>
-            </div>
-        );
-    }
-
-    if (error) {
-        return <p>Error: {error}</p>;
-    }
 
     return (
         <>
@@ -114,13 +54,17 @@ const MyShopPage = () => {
                 </div>
 
                 <div className="flex gap-8 ">
-                    {currentItems.map((item, index) => (
+                    {serviceQuery.data.map((item, index) => (
                         <div
                             key={index}
                             className="flex-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
                         >
                             <div className="w-full">
-                                <img className="rounded-t-lg w-full h-[240px]" src={item.imageURL} alt="" />
+                                <img
+                                    className="rounded-t-lg w-full h-[240px] object-cover"
+                                    src={item.imageURL}
+                                    alt=""
+                                />
                             </div>
                             <div className="p-5 ">
                                 <div>
@@ -131,30 +75,21 @@ const MyShopPage = () => {
                                 <p className="mb-3 font-normal leading-5 text-gray-700 dark:text-gray-400 line-clamp-2">
                                     {item.description}
                                 </p>
-                                <div className="flex my-4" key={item.id}>
-                                    {renderRatingStars(parseInt(item.avgRating))}
-                                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                                        {item?.rating?.toFixed(1)}
-                                    </span>
-                                </div>
-                                <div className="my-4 font-bold">{item.pricePerDay}$</div>
+
                                 <div className="flex justify-end">
                                     <Link
                                         to={{ pathname: `/item-detail-page/${item.id}` }}
                                         state={{ item }}
-                                        className="inline-flex items-center px-3 py-2 mb-10 mr-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                        className="inline-flex items-center px-10 py-2 mb-10 mr-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                     >
                                         Edit
                                     </Link>
                                     <div
-                                        className="inline-flex items-center px-3 py-2 mb-10 text-sm font-medium text-center text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                                        className="inline-flex items-center px-8 py-2 mb-10 text-sm font-medium text-center text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                                         onClick={() => handleDelete(item.id)}
                                     >
                                         Delete
                                     </div>
-                                </div>
-                                <div className="clear-both pb-5">
-                                    <span className="font-bold">Location: </span>
                                 </div>
                             </div>
                         </div>
@@ -162,17 +97,17 @@ const MyShopPage = () => {
                 </div>
                 {/* Pagination */}
                 <div className="flex items-center justify-center mt-6">
-                    {Array.from({ length: totalPages }, (_, index) => (
-                        <button
-                            key={index}
-                            className={`mr-2 p-2 rounded-full ${
-                                currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-blue-200 text-blue-800'
-                            }`}
-                            onClick={() => handlePageChange(index + 1)}
-                        >
-                            {index + 1}
-                        </button>
-                    ))}
+                    {totalPage > 1 && (
+                        <div className="col-span-4">
+                            <Pagination
+                                count={totalPage}
+                                page={page}
+                                onChange={(e, value) => {
+                                    setPage(value);
+                                }}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
         </>
